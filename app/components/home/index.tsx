64387a6b35af47221d72537cffdd7bb0c4391e8c
donellnjagix <74359807+donellnjagix@users.noreply.github.com>
2023-10-31 17:03:27 +0300

    2 images

--- app/components/home/index.tsx
+++ app/components/home/index.tsx
@@ -4,8 +4,6 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from "react-responsive-carousel";
 import image1 from "@/public/images/pic1.jpg";
 import image2 from "@/public/images/pic2.jpg";
-import image3 from "@/public/images/pic33.jpg";
-import image4 from '@/public/images/pic55.jpg';
 
 type Props = {};
 
@@ -44,30 +42,9 @@ const Homes = (props: Props) => {
             <p className="text-base md:text-lg pb-1 font-bold"> leading property developer in the Kenyan Real Estate Industry.</p>
           </div>
         </div>
-        <div className="w-full h-[60vh] sm:h-[90vh] relative">
-          <Image src={image3} alt="logo" fill className="object-cover" placeholder="blur" />
-          <div
-            className="absolute text-white top-[46%] md:left-[10%] p-[20px]
-           bg-gray-400 rounded backdrop-filter backdrop-blur-lg  bg-opacity-10"
-          >
-            <h1 className="font-bold text-2xl md:text-[30px] pt-3 px-8 text-alice-blue">
-              Welcome to Travaux ltd
-            </h1>
-            <p className="text-base md:text-lg pb-1 font-bold"> leading property developer in the Kenyan Real Estate Industry.</p>
-          </div>
-        </div>
-        <div className="w-full h-[60vh] sm:h-[90vh] relative">
-          <Image src={image4} alt="logo" fill className="object-cover" placeholder="blur" />
-          <div
-            className="absolute text-white top-[46%] md:left-[10%] p-[20px]
-           bg-gray-400 rounded backdrop-filter backdrop-blur-md bg-opacity-10"
-          >
-            <h1 className="font-bold text-2xl md:text-[30px] pt-3 px-8 text-alice-blue">
-              Welcome to Travaux ltd
-            </h1>
-            <p className="text-base md:text-lg pb-1 font-bold">leading property developer in the Kenyan Real Estate Industry.</p>
-          </div>
-        </div>
+        
+         
+        
       </Carousel>
     </div>
   );
