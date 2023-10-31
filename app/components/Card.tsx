4a5e6b2e7d4c93e0c88288c4e1002888c7594c2e
donellnjagix <74359807+donellnjagix@users.noreply.github.com>
2023-10-31 16:37:55 +0300

    Added 500 to width

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -20,7 +20,7 @@ const Card = ({
   longDescription,
 }: Props) => {
   return (
-    <div className="bg-alice-blue shadow-md p-4 flex items-center justify-center flex-col gap-2 text-center card-container" style={{ height: '400px' }}>
+    <div className="bg-alice-blue shadow-md p-4 flex items-center justify-center flex-col gap-2 text-center card-container" style={{ height: '500px' }}>
     <div className="relative h-60 w-full flex-shrink-0">
         {/* Map over the array of image strings and render each image */}
         {image.map((img, index) => (
