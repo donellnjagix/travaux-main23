90fd1824d9ac6dc221c69915f68b965be7c82def
donellnjagix <74359807+donellnjagix@users.noreply.github.com>
2023-11-01 07:09:58 +0300

    change te color

--- app/components/navbar/Nav.tsx
+++ app/components/navbar/Nav.tsx
@@ -6,9 +6,14 @@ import Image from 'next/image';
 type Props = {};
 
 const Nav = (props: Props) => {
+  const navStyle = {
+    fontFamily: 'Roboto',
+    backgroundColor: '#557ca9', // Set the background color to #557ca9
+  };
+
   return (
-    <nav style={{ fontFamily: 'Roboto' }}>
-      <div className="bg-alice-blue fixed top-0 z-30 w-full py-4 drop-shadow-md">
+    <nav style={navStyle}>
+      <div className="fixed top-0 z-30 w-full py-4 drop-shadow-md">
         <div className="flex items-center justify-between mx-auto w-5/6">
           <Link href="/">
             <Image src={Logo} alt="logo" height="70" width="70" className="object-cover scale-125" />
