8a3cc9591363fa80f268683be4890b4f75c08594
donellnjagix <74359807+donellnjagix@users.noreply.github.com>
2023-11-01 18:25:06 +0300

    try

--- app/components/navbar/Nav.tsx
+++ app/components/navbar/Nav.tsx
@@ -8,7 +8,7 @@ type Props = {};
 const Nav = (props: Props) => {
   return (
     <div>
-      {/* Navbar */}
+      
       <nav style={{ fontFamily: 'Roboto' }}>
         <div className="bg-alice-blue fixed top-0 z-30 w-full py-4 drop-shadow-md" style={{ backgroundColor: '#557ca9' }}>
           <div className="flex items-center justify-between mx-auto w-5/6">
