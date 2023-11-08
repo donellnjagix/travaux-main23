c8efe8820fec717b8b98d5f8ec689443d4751d9e
donellnjagix <74359807+donellnjagix@users.noreply.github.com>
2023-11-08 11:22:00 +0300

    Updated to add the media querry

--- app/components/home/index.tsx
+++ app/components/home/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import image1 from "@/public/images/pic1.jpg";
 import image2 from "@/public/images/pic2.jpg";
@@ -19,11 +19,8 @@ const Homes = (props: Props) => {
         interval={3000}
       >
         <div className="w-full h-[60vh] sm:h-[90vh] relative">
-          <Image src={image1} alt="logo" fill className="object-cover" placeholder="blur"/>
-          <div
-            className="absolute text-white top-[46%] md:left-[10%] p-[20px]
-           bg-gray-400 rounded backdrop-filter backdrop-blur-md bg-opacity-10"
-          >
+          <Image src={image1} alt="logo" fill className="object-cover" placeholder="blur" />
+          <div className="absolute text-white top-[46%] md:left-[10%] p-[20px] bg-gray-400 rounded backdrop-filter backdrop-blur-md bg-opacity-10">
             <h1 className="font-bold text-2xl md:text-[30px] pt-3 px-8 text-alice-blue">
               Welcome to Travaux ltd
             </h1>
@@ -31,20 +28,14 @@ const Homes = (props: Props) => {
           </div>
         </div>
         <div className="w-full h-[60vh] sm:h-[90vh] relative">
-          <Image src={image2} alt="logo" fill className="object-cover" placeholder="blur"/>
-          <div
-            className="absolute text-white top-[46%] md:left-[10%]
-           bg-gray-400 rounded backdrop-filter backdrop-blur-md bg-opacity-10 p-[20px]"
-          >
+          <Image src={image2} alt="logo" fill className="object-cover" placeholder="blur" />
+          <div className="absolute text-white top-[46%] md:left-[10%] p-[20px] bg-gray-400 rounded backdrop-filter backdrop-blur-md bg-opacity-10">
             <h1 className="font-bold text-2xl md:text-[30px] pt-3 px-8 text-alice-blue">
               Welcome to Travaux ltd
             </h1>
-            <p className="text-base md:text-lg pb-1 font-bold"> leading property developer in the Kenyan Real Estate Industry.</p>
+            <p className="text-base md:text-lg pb-1 font-bold">leading property developer in the Kenyan Real Estate Industry.</p>
           </div>
         </div>
-        
-         
-        
       </Carousel>
     </div>
   );
