f2866fd34552a45cef54cf1e7f9291ac57c1887a
donellnjagix <74359807+donellnjagix@users.noreply.github.com>
2023-11-23 12:19:38 +0300

    adjusted width

--- app/TopBar.tsx
+++ app/TopBar.tsx
@@ -9,9 +9,26 @@ const TopStrip: React.FC = () => {
       {' '}
       <a href="tel:+254729304190">(+254)-729-304-190</a>
       {' | '}
-      <a href="mailto:info@travauxlimited.com">example@email.com</a>
+      <a href="mailto:info@travauxlimited.com">info@travauxlimited.com</a>
+    </div>
+  );
+};
+
+export default TopStrip;a// components/TopStrip.tsx
+
+import React from 'react';
+
+const TopStrip: React.FC = () => {
+  return (
+    <div style={{ backgroundColor: 'orange', padding: '7px', textAlign: 'center', width: '100%', position: 'fixed', top: 0, left: 0, zIndex: 1000 }}>
+      Contacts: 
+      {' '}
+      <a href="tel:+254729304190">(+254)-729-304-190</a>
+      {' | '}
+      <a href="mailto:info@travauxlimited.com">info@travauxlimited.com</a>
     </div>
   );
 };
 
 export default TopStrip;
+
