cc5e53c93d78af727243c51912448242db0b2c55
donellnjagix <74359807+donellnjagix@users.noreply.github.com>
2023-11-23 20:24:35 +0300

    remove video

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,29 +1,16 @@
-// Import necessary libraries and components
-import React from 'react';
-import Video from 'next-video';
+import React from 'react'
 import FloatingWButton from "../components/WhatsAppButton";
 
-// Import the video file
-import awesomeVideo from '@/videos/video1.mp4';
-
-// Define the Props type
 type Props = {}
 
-// Create the page component
-const Page = (props: Props) => {
+const page = (props: Props) => {
   return (
     <div className='pt-20'>
-      {/* Display a title */}
-      <h1>Projects</h1>
-
-      {/* Display the video using the Video component */}
-      <Video src={awesomeVideo} />
-
-      {/* Add the WhatsApp floating button */}
-      <FloatingWButton />
+        Projects
+        <FloatingWButton />
     </div>
-  );
+   
+  )
 }
 
-// Export the page component
-export default Page;
+export default page
