063d892968daba4f38cfb489b36f3b5d0afec345
donellnjagix <74359807+donellnjagix@users.noreply.github.com>
2023-11-23 12:28:29 +0300

    try padding

--- app/TopBar.tsx
+++ app/TopBar.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const TopStrip: React.FC = () => {
   return (
-    <div style={{ backgroundColor: 'orange', padding: '7px', textAlign: 'center' }}>
+    <div style={{ backgroundColor: 'orange', padding: '7px', textAlign: 'center', width: '100%', position: 'fixed', top: 0, left: 0, zIndex: 1000 }}>
       Contacts: 
       {' '}
       <a href="tel:+254729304190">(+254)-729-304-190</a>
